e5e860415f899dd85158b2d28d404e4f035a5bb6
AleChv <chivualexandramaria02@gmail.com>
2025-03-28 17:44:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IEmitSound
+{
+    void EmitSound();
+}
+
+//The properties animalType and sound are hidden and can only be accessed or modified through the class methods
+class Animal : IEmitSound
+{
+    public string AnimalType { get; set; }
+    public string Sound { get; set; }
+
+    public Animal(string animalType, string sound)
+    {
+        AnimalType = animalType;
+        Sound = sound;
+    }
+
+    public void EmitSound()
+    {
+        Console.WriteLine($"{AnimalType} makes sound: {Sound}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //the list of objects that implements the IEmitSound interface
+        List<IEmitSound> soundEmitters = new List<IEmitSound>
+        {
+            new Animal("Dog", "Bark"),
+            new Animal("Cat", "Meow"),
+            new Animal("Cow", "Moo"),
+            new Animal("Sheep", "Baa"),
+            new Animal("Duck", "Quack")
+        };
+
+        foreach (var soundEmitter in soundEmitters)
+        {
+            soundEmitter.EmitSound();
+        }
+    }
+}
